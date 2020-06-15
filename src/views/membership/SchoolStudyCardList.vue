fc704a97cd00f1159d55acf6dab1bb155a41a565
mac <mac@macdeMacBook-Pro.local>
2020-06-15 22:31:49 +0800

    测试一下

--- src/views/membership/SchoolStudyCardList.vue
+++ src/views/membership/SchoolStudyCardList.vue
@@ -17,23 +17,57 @@
         :default-sort="tableConfig.defalutSort"
         :style="tableConfig.style"
       >
-        <el-table-column align="center" prop="cardCode" label="学习卡编码" width />
-        <el-table-column align="center" prop="cardErcode" label="二维码" show-overflow-tooltip />
+        <el-table-column
+          align="center"
+          prop="cardCode"
+          label="学习卡编码"
+          width
+        />
+        <el-table-column
+          align="center"
+          prop="cardErcode"
+          label="二维码"
+          show-overflow-tooltip
+        />
         <el-table-column
           align="center"
           prop="cardType"
           label="学习卡类型"
           :formatter="cardTypeFormatter"
         />
-        <el-table-column align="center" prop="isBind" label="绑定状态" :formatter="bindFormatter" />
-        <el-table-column align="center" prop="validityMonth" label="有效期">
+        <el-table-column
+          align="center"
+          prop="isBind"
+          label="绑定状态"
+          :formatter="bindFormatter"
+        />
+        <el-table-column
+          align="center"
+          prop="validityMonth"
+          label="有效期"
+        >
           <template slot-scope="scope">
             <span>{{ scope.row.validityMonth + '个月' }}</span>
           </template>
         </el-table-column>
-        <el-table-column align="center" prop="createTime" label="创建时间" show-overflow-tooltip />
-        <el-table-column align="center" prop="endTime" label="到期时间" :formatter="endTimeFormatter" />
-        <el-table-column align="center" prop="status" label="状态" :formatter="statusFormatter" />
+        <el-table-column
+          align="center"
+          prop="createTime"
+          label="创建时间"
+          show-overflow-tooltip
+        />
+        <el-table-column
+          align="center"
+          prop="endTime"
+          label="到期时间"
+          :formatter="endTimeFormatter"
+        />
+        <el-table-column
+          align="center"
+          prop="status"
+          label="状态"
+          :formatter="statusFormatter"
+        />
       </el-table>
     </el-card>
     <table-foot
